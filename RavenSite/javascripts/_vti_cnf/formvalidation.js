9e0fb610e130567675969db7e127cce79df69a38
LordOfDarknessAlexander <alexandermagus6@icloud.com>
2015-04-08 20:49:01 -0400

    .

---
+++ RavenSite/javascripts/_vti_cnf/formvalidation.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|02 Dec 2014 15:45:31 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|AOSite/theme/contact.html AOSite/theme/about.html AOSite/news.html AOSite/theme/services.html
+vti_author:SR|ALEXS\\TzL
+vti_modifiedby:SR|ALEXS\\TzL
+vti_timecreated:TR|02 Dec 2014 15:45:31 -0000
+vti_cacheddtm:TX|02 Dec 2014 15:45:31 -0000
+vti_filesize:IR|2156
---
+++ RavenSite/javascripts/_vti_cnf/scrolltotop.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|02 Dec 2014 15:45:32 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|AOSite/detail.html AOSite/theme/about.html AOSite/theme/blogsinglepost.html AOSite/theme/portofolio2.html AOSite/theme/portofolio3.html AOSite/theme/portofolio4.html AOSite/news.html AOSite/tutorial.html AOSite/theme/columns.html AOSite/theme/typography.html AOSite/game.html AOSite/theme/index.html AOSite/theme/services.html AOSite/theme/portofoliodetail.html AOSite/showroom.html AOSite/blogpage.html AOSite/theme/blogpage1.html AOSite/theme/blogpage2.html AOSite/theme/blogpage3.html AOSite/theme/contact.html AOSite/theme/elements.html
